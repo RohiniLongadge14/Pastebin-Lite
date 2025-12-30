a248300079f260d57458551835934aed9dadbf30
RohiniLongadge14 <rohinilon875@gmail.com>
2025-12-30 16:08:44 +0530

    Fix CORS to allow Netlify frontend

--- src/main/java/io/reflectoring/pastebinlite/config/CorsConfig.java
+++ src/main/java/io/reflectoring/pastebinlite/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package io.reflectoring.pastebinlite.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
